ce03f634ce6964bb2e3df44648f94d20f597fd3f
Denis Olifer <denis@olifer.org>
2021-08-24 23:15:53 +0300

    Add initial implementation of Query and Mutations
    Basic coverage with tests

---
+++ src/Core/Mutation.cs
@@ -0,0 +1,14 @@
+﻿using NGql.Core.Abstractions;
+
+namespace NGql.Core
+{
+    public sealed class Mutation : QueryPart
+    {
+        public Mutation(string name)
+            : base(name)
+        {
+        }
+
+        protected override string Prefix => "mutation";
+    }
+}
---
+++ tests/Core.Tests/MutationTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using NGql.Core;
+using Xunit;
+
+namespace Core.Tests
+{
+    public class MutationTests
+    {
+        [Fact]
+        public void ToString_Returns_SubQuery()
+        {
+            // arrange
+            var mutation = new Mutation("CreateUser");
+            var nestedMutation = new Query("createUser")
+                .Where("name", "Name")
+                .Where("password", "Password")
+                .Select("id", "name");
+
+            // act
+            var queryText = mutation
+                .Select(nestedMutation)
+                .ToString();
+
+            // assert
+            queryText.Should().Be(@"mutation CreateUser{
+    createUser(name:""Name"", password:""Password""){
+        id
+        name
+    }
+}");
+        }
+    }
+}
