0b9655c8a7381368e4c31778982195b9e24696c1
Denis Olifer <denis@olifer.org>
2021-09-09 21:48:52 +0300

    Add more benchmarks

---
+++ tests/BenchmarkRunner/Benchmarks/StringBuilderBenchmark.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class StringBuilderBenchmark
+    {
+        [Benchmark]
+        public string AppendChar()
+            => new StringBuilder().Append('(').ToString();
+
+        [Benchmark]
+        public string AppendString()
+            => new StringBuilder().Append("(").ToString();
+    }
+}
