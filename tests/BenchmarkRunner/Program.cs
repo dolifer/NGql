dd0813b3ddbfd25ddcadc794140fc41b2d361c56
Denis Olifer <denis@olifer.org>
2021-09-09 20:59:09 +0300

    refactor: feat(benchmarks): add benchmarks

---
+++ tests/BenchmarkRunner/Program.cs
@@ -0,0 +1,7 @@
+﻿using BenchmarkDotNet.Running;
+
+public class Program
+{
+   public static void Main(string[] args)
+       => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+}
