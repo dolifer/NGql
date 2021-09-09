9d5e2cf74d2e430b4e9b7b5b52695dabb496bebc
Denis Olifer <denis@olifer.org>
2021-09-09 20:59:09 +0300

    Add benchmark runner
    Add query benchmark

---
+++ tests/BenchmarkRunner/Benchmarks/QueryBuilderBenchmark.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using GraphQL;
+using NGql.Core;
+
+namespace Benchmarks.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class QueryBuilderBenchmark
+    {
+        private readonly string _queryText;
+
+        public QueryBuilderBenchmark() => _queryText = GetQuery();
+
+        [Benchmark]
+        public GraphQLRequest GetStatic() => new()
+        {
+            Query = _queryText,
+            Variables = new
+            {
+                date = DateTimeOffset.UtcNow
+            }
+        };
+
+        [Benchmark]
+        public GraphQLRequest GetNew() => new()
+        {
+            Query = GetQuery(),
+            Variables = new
+            {
+                date = DateTimeOffset.UtcNow
+            }
+        };
+
+        private static Query GetQuery() => new Query("PersonAndFilms")
+            .Select(new Query("person")
+                .Where("id", "cGVvcGxlOjE=")
+                .Select("name")
+                .Select(new Query("filmConnection")
+                    .Select(new Query("films")
+                        .Select("title")))
+            );
+    }
+}
