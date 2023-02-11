809cba5d92c806b6f59d3670910fc0a9ace6cc88
Denis Olifer <denis@olifer.org>
2023-02-11 22:17:53 +0200

    Reuse StringBuilder instance
    Close #5

---
+++ src/Core/ValueFormatter.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Globalization;
+
+namespace NGql.Core;
+
+internal sealed class ValueFormatter
+{
+    internal static bool TryFormatPrimitiveType(object value, out string? stringValue)
+    {
+        stringValue = value switch
+        {
+            string s => $"\"{s}\"",
+            bool boolValue => boolValue.ToString(CultureInfo.InvariantCulture).ToLowerInvariant(),
+            byte byteValue => byteValue.ToString(CultureInfo.InvariantCulture),
+            sbyte sbyteValue => sbyteValue.ToString(CultureInfo.InvariantCulture),
+            short shortValue => shortValue.ToString(CultureInfo.InvariantCulture),
+            ushort ushortValue => ushortValue.ToString(CultureInfo.InvariantCulture),
+            int intValue => intValue.ToString(CultureInfo.InvariantCulture),
+            uint uintValue => uintValue.ToString(CultureInfo.InvariantCulture),
+            long longValue => longValue.ToString(CultureInfo.InvariantCulture),
+            ulong ulongValue => ulongValue.ToString(CultureInfo.InvariantCulture),
+            float floatValue => floatValue.ToString(CultureInfo.InvariantCulture),
+            double doubleValue => doubleValue.ToString(CultureInfo.InvariantCulture),
+            decimal decimalValue => decimalValue.ToString(CultureInfo.InvariantCulture),
+            Enum enumValue => enumValue.ToString(),
+            Variable variable => variable.Name,
+            _ => default
+        };
+
+        return !string.IsNullOrWhiteSpace(stringValue);
+    }
+}
