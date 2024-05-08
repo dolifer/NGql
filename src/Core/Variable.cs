bdd427c6328ed47da88293ed9edeed3971398676
Denis Olifer <denis@olifer.org>
2024-05-08 08:51:28 +0300

    feat: auto discover variables from fields, arguments and nested...

--- src/Core/Variable.cs
+++ src/Core/Variable.cs
@@ -1,14 +1,66 @@
+using System;
+
 namespace NGql.Core
 {
-    public readonly struct Variable
+    /// <summary>
+    ///     Represents a variable in GraphQL query.
+    /// </summary>
+    public readonly struct Variable : IComparable, IComparable<Variable>
     {
+        /// <summary>
+        /// Name of the variable.
+        /// </summary>
         public string Name { get; }
+
+        /// <summary>
+        /// Type of the variable.
+        /// </summary>
         public string Type { get; }
 
         public Variable(string name, string type)
         {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Variable name cannot be null or whitespace.", nameof(name));
+
+            if (!name.StartsWith('$'))
+                throw new ArgumentException("Variable name must start with '$'.", nameof(name));
+
+            if (string.IsNullOrWhiteSpace(type))
+                throw new ArgumentException("Variable type cannot be null or whitespace.", nameof(type));
+
             Name = name;
             Type = type;
         }
+
+        public override string ToString() => $"{Name}:{Type}";
+
+        public int CompareTo(object? obj)
+        {
+            return obj switch
+            {
+                null => 1,
+                Variable variable => CompareTo(variable),
+                _ => throw new ArgumentException($"Object must be of type {nameof(Variable)}")
+            };
+        }
+
+        public int CompareTo(Variable other)
+        {
+            var nameComparison = string.Compare(Name, other.Name, StringComparison.Ordinal);
+            return nameComparison != 0 ? nameComparison : string.Compare(Type, other.Type, StringComparison.Ordinal);
+        }
+        
+        public static bool operator ==(Variable left, Variable right) => left.Equals(right);
+        public static bool operator !=(Variable left, Variable right) => !left.Equals(right);
+        
+        public static bool operator <(Variable left, Variable right) => left.CompareTo(right) < 0;
+        public static bool operator >(Variable left, Variable right) => left.CompareTo(right) > 0;
+        
+        public static bool operator <=(Variable left, Variable right) => left.CompareTo(right) <= 0;
+        public static bool operator >=(Variable left, Variable right) => left.CompareTo(right) >= 0;
+        
+        public override bool Equals(object? obj) => CompareTo(obj) == 0;
+        
+        public override int GetHashCode() => HashCode.Combine(Name, Type);
     }
 }
---
+++ tests/Core.Tests/VariableTests.cs
@@ -0,0 +1,105 @@
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace NGql.Core.Tests;
+
+public class VariableTests
+{
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void Test_Create_Bad_Type(string type)
+    {
+        var exception = Assert.Throws<ArgumentException>(() => new Variable("$name", type));
+        
+        exception.Message.Should().Be("Variable type cannot be null or whitespace. (Parameter 'type')");
+    }
+    
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void Test_Create_Bad_Name(string name)
+    {
+        var exception = Assert.Throws<ArgumentException>(() => new Variable(name, "String"));
+        
+        exception.Message.Should().Be("Variable name cannot be null or whitespace. (Parameter 'name')");
+    }
+    
+    [Fact]
+    public void Test_Create_StartsWithDollarSign()
+    {
+        var exception = Assert.Throws<ArgumentException>(() => new Variable("name", "String"));
+        
+        exception.Message.Should().Be("Variable name must start with '$'. (Parameter 'name')");
+    }
+    
+    [Fact]
+    public void Test_CompareTo()
+    {
+        var name = new Variable("$name", "String");
+        
+        var exception = Assert.Throws<ArgumentException>(() => name.CompareTo(new{}));
+        
+        exception.Message.Should().Be("Object must be of type Variable");
+    }
+
+    [Fact]
+    public void Test_ToString()
+    {
+        var name = new Variable("$name", "String");
+        var age = new Variable("$age", "Int");
+        
+        name.ToString().Should().Be("$name:String");
+        age.ToString().Should().Be("$age:Int");
+    }
+
+    [Fact]
+    public void Test_Equals()
+    {
+        var name1 = new Variable("$name", "String");
+        var name2 = new Variable("$name", "String");
+        var name3Int = new Variable("$name", "Int");
+
+        name1.Equals(name2).Should().BeTrue();
+        name2.Equals(name1).Should().BeTrue();
+
+        name1.Equals(name3Int).Should().BeFalse();
+        name2.Equals(name3Int).Should().BeFalse();
+        
+        (name1 == name2).Should().BeTrue();
+        (name2 == name1).Should().BeTrue();
+        
+        (name1 != name3Int).Should().BeTrue();
+        (name2 != name3Int).Should().BeTrue();
+        
+        (name3Int != name1).Should().BeTrue();
+        (name3Int != name2).Should().BeTrue();
+    }
+    
+    // ReSharper disable EqualExpressionComparison
+    [Fact]
+    public void Test_Equality()
+    {
+        var a = new Variable("$a", "String");
+        var b = new Variable("$b", "String");
+
+        (a != a).Should().BeFalse();
+        
+        (a > a).Should().BeFalse();
+        (a < a).Should().BeFalse();
+
+        (a > b).Should().BeFalse();
+        (a >= b).Should().BeFalse();
+        
+        (a == a).Should().BeTrue();
+        
+        (a >= a).Should().BeTrue();
+        (a <= a).Should().BeTrue();
+        
+        (a < b).Should().BeTrue();
+        (a <= b).Should().BeTrue();
+    }
+}
