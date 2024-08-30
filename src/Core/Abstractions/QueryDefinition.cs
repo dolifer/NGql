8a763e35e303811cadca5c48653b44b0c88810e3
Denis Olifer <denis@olifer.org>
2024-08-30 12:24:32 +0300

    feat: add query definition and field definitions

---
+++ src/Core/Abstractions/QueryDefinition.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace NGql.Core.Abstractions;
+
+/// <summary>
+///     Represents a query definition.
+/// </summary>
+/// <param name="Name">The name of the query.</param>
+public sealed record QueryDefinition([property:JsonPropertyName("name")] string Name)
+{
+    /// <summary>
+    ///     The collection of fields related to <see cref="QueryDefinition"/>.
+    /// </summary>
+    [JsonPropertyName("fields")]
+    public Dictionary<string, FieldDefinition> Fields { get; } = new();
+}
---
+++ src/Core/QueryBuilder.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using NGql.Core.Abstractions;
+using NGql.Core.Extensions;
+
+namespace NGql.Core;
+
+/// <summary>
+///     Represents a query builder.
+/// </summary>
+public sealed class QueryBuilder
+{
+    private readonly QueryDefinition _queryDefinition;
+
+    private QueryBuilder(QueryDefinition queryDefinition) => _queryDefinition = queryDefinition;
+
+    /// <summary>
+    ///     Creates a new instance of <see cref="QueryBuilder"/>.
+    /// </summary>
+    /// <param name="name">The name of the query.</param>
+    /// <returns>Instance of <see cref="QueryBuilder"/>.</returns>
+    public static QueryBuilder New(string name) => new(new QueryDefinition(name));
+    
+    /// <summary>
+    ///     Creates a new instance of <see cref="QueryBuilder"/>.
+    /// </summary>
+    /// <param name="queryDefinition">The query definition.</param>
+    /// <returns>Instance of <see cref="QueryBuilder"/>.</returns>
+    public static QueryBuilder FromDefinition(QueryDefinition queryDefinition) => new(queryDefinition);
+
+    /// <summary>
+    ///     Adds a field to the query.
+    /// </summary>
+    /// <param name="field">Field name or path.</param>
+    /// <returns>Instance of <see cref="QueryBuilder"/>.</returns>
+    /// <exception cref="ArgumentException">Thrown when the field is null or empty.</exception>
+    public QueryBuilder AddField(string field)
+    {
+        if (string.IsNullOrWhiteSpace(field))
+        {
+            throw new ArgumentException("Field cannot be null or empty", nameof(field));
+        }
+        
+        var parts = field.Split('.', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        var value = GetField(_queryDefinition.Fields, parts);
+
+        AddSubField(value, parts[1..]);
+        return this;
+    }
+
+    private static void AddSubField(FieldDefinition parent, string[] path)
+    {
+        if (path.Length == 0)
+        {
+            return;
+        }
+        
+        var value = GetField(parent.Fields, path);
+
+        AddSubField(value, path[1..]);
+    }
+
+    private static FieldDefinition GetField(Dictionary<string, FieldDefinition> fields, string[] path)
+    {
+        var first = path[0];
+
+        if (fields.TryGetValue(first, out var value))
+        {
+            return value;
+        }
+
+        return fields[first] = new FieldDefinition(first);
+    }
+    
+    public Query ToQuery() => _queryDefinition.ToQuery();
+}
